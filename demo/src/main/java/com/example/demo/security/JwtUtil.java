f8d0b2313451cc5dde995206b8967900dd67ff4b
Moubtahij Otmane <otman.moubtahij15@gmail.com>
2022-12-22 14:10:09 +0100

    EXPIRE_ACCESS_TOKEN augmente

--- demo/src/main/java/com/example/demo/security/JwtUtil.java
+++ demo/src/main/java/com/example/demo/security/JwtUtil.java
@@ -4,7 +4,7 @@ public class JwtUtil {
     public static final String SECRET = "secret";
     public static final String AUTH_HEADER = "Authorization";
     public static final String PREFIX = "Bearer ";
-    public static final long EXPIRE_ACCESS_TOKEN = 2 * 60 * 1000;
+    public static final long EXPIRE_ACCESS_TOKEN = 100 * 60 * 1000;
     public static final long EXPIRE_ACCESS_refresh = 2 * 60 * 1000;
 
 
