11161e98504fe1c976379a5eae1bb75d95626c59
Moubtahij Otmane <77882695+moubtahij15@users.noreply.github.com>
2023-01-27 12:08:57 +0100

    Update CorsConfig.java

--- demo/src/main/java/com/example/demo/security/CorsConfig.java
+++ demo/src/main/java/com/example/demo/security/CorsConfig.java
@@ -17,7 +17,7 @@ public class CorsConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("https://moubtahij15.github.io"));
+        configuration.setAllowedOrigins(Arrays.asList("https://bcdmdist.up.railway.app"));
         configuration.setAllowedMethods(Arrays.asList("GET","POST","OPTIONS", "PUT"));
         configuration.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization"));
         configuration.setAllowCredentials(true);
@@ -31,7 +31,7 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration().applyPermitDefaultValues();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(Arrays.asList("https://moubtahij15.github.io"));
+        config.setAllowedOrigins(Arrays.asList("https://bcdmdist.up.railway.app"));
         config.setAllowedMethods(Arrays.asList("*"));
         config.setAllowedHeaders(Arrays.asList("*"));
         source.registerCorsConfiguration("/**", config);
